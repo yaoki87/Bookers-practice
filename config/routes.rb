f40487c21048df351a0ae8d94d7764d71ab56101
yaoki87 <878787yao@gmail.com>
2022-02-06 14:00:04 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-get 'top' => 'homes#top'
-resources :books
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  root to: 'homes#top'
 end
